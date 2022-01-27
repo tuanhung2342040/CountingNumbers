215b54cff95d7d65231866a78fcb05b3aa28e0fe
Hung <tuanhung082018@gmail.com>
2022-01-26 21:19:10 -0800

    1

---
+++ HTML and JavaScript/functions.js
@@ -0,0 +1,34 @@
+function sumOfThreeInt(num1, num2, num3){
+    return num1 + num2 + num3;
+}
+/*console.log(sumOfThreeInt(1, 2, 3));
+let a = Math.ceil((Math.random() * 5) + 1);
+for(i = 0; i <= 5;i++){
+    console.log(Math.ceil((Math.random() * 5) + 1));
+}
+let str1 = 'Today is a good day.';
+console.log(str1.length);
+console.log(str1.indexOf('o'));
+console.log(str1.lastIndexOf('o'));*/
+let x = 3;
+let y = 4;
+function sumOfSquare(x, y){
+    return x**2+y**2;   
+    // or Math.pow(x,2) + Math.pow(y,2);
+}
+console.log(sumOfSquare(x, y));
+
+let randomString = 'JS1011';
+function shortenString(str){
+    return str.substring(0, 2);
+}
+console.log(shortenString(randomString));
+console.log(false > 0);
+console.log(!false > 0);
+console.log(true > 0);
+
+
+
+
+
+
