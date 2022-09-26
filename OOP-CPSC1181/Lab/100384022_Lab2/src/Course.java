6bfdd335f6c0cbf335c74cf88c3139fd4dead1c4
Hung <tuanhung082018@gmail.com>
2022-09-26 15:57:59 -0700

    Add comments

--- OOP-CPSC1181/Lab/100384022_Lab2/src/Course.java
+++ OOP-CPSC1181/Lab/100384022_Lab2/src/Course.java
@@ -2,15 +2,28 @@ import java.util.ArrayList;
 
 public class Course {
    private ArrayList<Student> students; 
-   
+   /**
+    * This constructs a course with ArrayList of students
+    */
    public Course() {
       students = new ArrayList<>();  
    }
+   /**
+    * This adds a new student to the Course
+    * @param name the name of the new student
+    * @param familyName the family name of the new student
+    * @return reference of the new student
+    */
    public Student addStudent(String name, String familyName){
       Student newStudent = new Student(name, familyName);
       students.add(newStudent);
       return newStudent; 
    }
+   /**
+    * This finds a student in the course with a given student number.
+    * @param studentNumber the student number of the student
+    * @return reference of the student
+    */
    public Student findStudent(long studentNumber){
       for(Student s : students){
          if(s.getStudentNumber() == studentNumber){
@@ -19,6 +32,11 @@ public class Course {
       }
       return null;
    }
+   /**
+    * This removes a student from the course with a given student number.
+    * @param studentNumber the student number of the student
+    * @return reference of the student 
+    */
    public Student deleteStudent(long studentNumber){
       if(students.isEmpty()){
          return null;
@@ -31,6 +49,14 @@ public class Course {
       }
       return null;
    }
+   /**
+    * This adds a quiz of the student 
+    * @param studentNumber the student number of the student
+    * @param maxGrade the maximum grade of the quiz
+    * @param studentGrade the grade of the student
+    * @return true if the given student number 
+    matches with the student, otherwise return false
+    */
    public boolean addQuiz(long studentNumber, double maxGrade, double studentGrade){
       if(maxGrade <= 0 || studentGrade < 0 || studentNumber < 10000001 || 
              maxGrade < studentGrade){
@@ -44,6 +70,10 @@ public class Course {
       }
       return false;
    }
+   /**
+    * This returns the top student of the course
+    * @return reference of the top student
+    */
    public Student findTopStudent(){
       if(students.isEmpty()){
          return null;
@@ -56,6 +86,10 @@ public class Course {
       }
       return topStudent;
    }
+   /**
+    * This return the average grade of all students of the class
+    * @return average grade
+    */
    public double getAverage(){
       int length = students.size();
       double sum = 0;
--- OOP-CPSC1181/Lab/100384022_Lab2/src/Quiz.java
+++ OOP-CPSC1181/Lab/100384022_Lab2/src/Quiz.java
@@ -1,7 +1,11 @@
 public class Quiz {
     private double maxGrade; 
     private double studentGrade;
-
+    /**
+     * This constructs a quiz with max grade and student grade
+     * @param maxGrade max grade of the quiz
+     * @param studentGrade student's grade
+     */ 
     public Quiz(double maxGrade, double studentGrade){
         this.maxGrade = maxGrade;
         this.studentGrade = studentGrade;
